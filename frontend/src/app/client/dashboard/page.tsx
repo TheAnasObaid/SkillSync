02fccdcdae54c80a49fc60c5b55c05026eca4595
Faizanali360 <alif32398@gmail.com>
2025-07-24 19:27:06 +0500

    Build static client dashboard page

--- frontend/src/app/client/dashboard/page.tsx
+++ frontend/src/app/client/dashboard/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import ProtectedRoute from "@/components/Auth/ProtectedRoute";
+import Image from "next/image";
 import Link from "next/link";
 
 export interface Challenge {
@@ -13,11 +16,55 @@ export interface Challenge {
 const Dashboard = () => {
   return (
     <ProtectedRoute requiredRole="client">
-      <div className="grid gap-5">
-        <h2 className="text-2xl font-bold">Client</h2>
-        <Link href="/challenges/create" className="btn btn-secondary">
-          Create Challenge
-        </Link>
+      <div className="grid grid-cols-4 gap-5">
+        <aside className="p-5">
+          <ul className="menu">
+            <li>
+              <Link href="#">General</Link>
+            </li>
+            <li>
+              <Link href="#">About</Link>
+            </li>
+            <li>
+              <Link href="/challenges/create">Create Challenge</Link>
+            </li>
+          </ul>
+        </aside>
+        <div className="col-span-3 p-5">
+          <div className="flex flex-col items-center gap-4 max-w-sm mx-auto">
+            <div className="flex items-center gap-5 ">
+              <Image
+                src="https://thispersondoesnotexist.com/"
+                alt="Profile"
+                width={80}
+                height={80}
+                className="rounded-full object-cover"
+              />
+              <button className="btn btn-secondary btn-sm">Update image</button>
+            </div>
+
+            <form className="grid gap-5 w-full">
+              <div className="grid gap-2">
+                <label className="label font-semibold">Username</label>
+                <input className="input w-full" type="text" />
+              </div>
+              <div className="grid gap-2">
+                <label className="label font-semibold">Email</label>
+                <input className="input w-full" type="text" />
+              </div>
+              <div className="grid gap-2">
+                <label className="label font-semibold">Bio</label>
+                <textarea className="textarea w-full" />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-secondary btn-outline w-fit"
+              >
+                Done
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </ProtectedRoute>
   );
