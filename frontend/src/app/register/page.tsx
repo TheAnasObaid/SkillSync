61d21e17f6651a588aed80ef61a674e1554e704e
Anas Obaid <anasobaidpk@gmail.com>
2025-07-27 18:56:36 +0500

    Add back buttons to auth forms

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,8 +1,13 @@
 import RegisterForm from "@/components/Auth/RegisterForm";
+import Link from "next/link";
+import { IoReturnUpBack } from "react-icons/io5";
 
 const RegisterPage = () => {
   return (
     <div className="h-screen grid grid-rows-[auto_1fr_auto] my-5">
+      <Link href="/" className="w-fit tab justify-start">
+        <IoReturnUpBack size={24} />
+      </Link>
       <div className="grid gap-5 max-w-sm w-full mx-auto">
         <h2 className="text-4xl font-semibold">Register</h2>
         <RegisterForm />
