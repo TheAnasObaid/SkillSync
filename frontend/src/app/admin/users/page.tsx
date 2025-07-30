3d90f07387801eb097624d832ac12e72d8f41a52
Anas Obaid <anasobaidpk@gmail.com>
2025-07-30 13:00:14 +0500

    Make the status table cell more informative

--- frontend/src/app/admin/users/page.tsx
+++ frontend/src/app/admin/users/page.tsx
@@ -5,12 +5,13 @@ import DashboardLayout, {
 } from "@/components/Layout/DashboardLayout";
 import UserAvatar from "@/components/Profile/UserAvatar";
 import apiClient from "@/services/apiClient";
-import { User } from "@/store/authStore";
+import { useAuthStore, User } from "@/store/authStore";
 import { useEffect, useState } from "react";
 import {
   FiClipboard,
   FiGrid,
   FiMoreVertical,
+  FiSlash,
   FiUser,
   FiUsers,
 } from "react-icons/fi";
@@ -26,6 +27,8 @@ const ManageUsersPage = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+
+  const { user: currentUser } = useAuthStore();
 
   const fetchUsers = () => {
     apiClient
@@ -46,7 +49,7 @@ const ManageUsersPage = () => {
       await apiClient.patch(`/admin/users/${userId}`, updates);
       fetchUsers();
     } catch (err) {
-      setError("Failed to update user!.");
+      setError("Failed to update user.");
     }
   };
 
@@ -72,138 +75,187 @@ const ManageUsersPage = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
-              <tr key={user!._id} className="hover">
-                <td>
-                  <div className="flex items-center gap-3">
-                    <UserAvatar
-                      name={user!.profile?.firstName}
-                      avatarUrl={user!.profile?.avatar}
-                    />
-                    <div className="overflow-hidden">
-                      <div className="font-bold truncate">
-                        {user!.profile?.firstName} {user!.profile?.lastName}
-                      </div>
-                      <div className="text-sm text-base-content/40 truncate">
-                        {user!.email}
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <span className="badge badge-ghost badge-sm">
-                    {user!.role}
-                  </span>
-                </td>
-                <td>
-                  {user!.isVerified ? (
-                    <span className="badge badge-success badge-soft">
-                      Verified
-                    </span>
-                  ) : (
-                    <span className="badge badge-warning badge-soft">
-                      Not Verified
-                    </span>
-                  )}
-                </td>
-                <td>{new Date(user!.createdAt).toLocaleDateString()}</td>
+            {users.map((user) => {
+              const isCurrentUser = currentUser?._id === user?._id;
 
-                <th>
-                  <div className="dropdown dropdown-end">
-                    <button tabIndex={0} className="btn btn-ghost btn-xs">
-                      <FiMoreVertical />
-                    </button>
-                    <ul
-                      tabIndex={0}
-                      className="dropdown-content z-[1] menu p-2 shadow bg-base-100 font-normal border border-base-300 rounded-box w-52"
-                    >
-                      {!user!.isVerified && (
-                        <li>
-                          <a
-                            onClick={() =>
-                              handleUpdateUser(user!._id, {
-                                isVerified: true,
-                              })
-                            }
-                          >
-                            Mark as Verified
-                          </a>
-                        </li>
-                      )}
-                      {user!.role !== "admin" && (
-                        <>
-                          <li>
-                            <a
-                              onClick={() =>
-                                handleUpdateUser(user!._id, {
-                                  role: "admin",
-                                })
-                              }
-                            >
-                              Promote to Admin
-                            </a>
-                          </li>
-                          {user!.role === "client" && (
-                            <li>
-                              <a
-                                onClick={() =>
-                                  handleUpdateUser(user!._id, {
-                                    role: "developer",
-                                  })
-                                }
-                              >
-                                Change to Developer
-                              </a>
-                            </li>
-                          )}
-                          {user!.role === "developer" && (
-                            <li>
-                              <a
-                                onClick={() =>
-                                  handleUpdateUser(user!._id, {
-                                    role: "client",
-                                  })
-                                }
-                              >
-                                Change to Client
-                              </a>
-                            </li>
-                          )}
-                        </>
-                      )}
+              if (user)
+                return (
+                  <tr key={user._id} className="hover">
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <UserAvatar
+                          name={user.profile?.firstName}
+                          avatarUrl={user.profile?.avatar}
+                        />
+                        <div className="overflow-hidden">
+                          <div className="font-bold truncate">
+                            {user.profile?.firstName} {user.profile?.lastName}
+                          </div>
+                          <div className="text-sm text-base-content/40 truncate">
+                            {user.email}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="badge badge-ghost badge-sm">
+                        {user.role}
+                      </span>
+                    </td>
+                    <td>
+                      <div className="flex flex-col gap-2">
+                        {user.accountStatus === "active" ? (
+                          <div className="badge badge-xs badge-success badge-soft">
+                            Active
+                          </div>
+                        ) : (
+                          <div className="badge badge-xs badge-error badge-soft">
+                            Banned
+                          </div>
+                        )}
+                        {user.isVerified ? (
+                          <div className="badge badge-xs badge-info badge-soft">
+                            Verified
+                          </div>
+                        ) : (
+                          <div className="badge badge-xs badge-warning badge-soft">
+                            Not Verified
+                          </div>
+                        )}
+                      </div>
+                    </td>
+                    <td>{new Date(user.createdAt).toLocaleDateString()}</td>
 
-                      {user!.accountStatus === "active" ? (
-                        <li>
-                          <a
-                            className="text-error"
-                            onClick={() =>
-                              handleUpdateUser(user!._id, {
-                                accountStatus: "banned",
-                              })
-                            }
+                    <th>
+                      <div className="dropdown dropdown-end">
+                        <button
+                          tabIndex={0}
+                          className="btn btn-ghost btn-xs"
+                          disabled={isCurrentUser}
+                        >
+                          {isCurrentUser ? <FiSlash /> : <FiMoreVertical />}
+                        </button>
+                        {!isCurrentUser && (
+                          <ul
+                            tabIndex={0}
+                            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
                           >
-                            Ban User
-                          </a>
-                        </li>
-                      ) : (
-                        <li>
-                          <a
-                            className="text-success"
-                            onClick={() =>
-                              handleUpdateUser(user!._id, {
-                                accountStatus: "active",
-                              })
-                            }
-                          >
-                            Unban User
-                          </a>
-                        </li>
-                      )}
-                    </ul>
-                  </div>
-                </th>
-              </tr>
-            ))}
+                            {!user.isVerified && (
+                              <li>
+                                <a
+                                  onClick={() =>
+                                    handleUpdateUser(user._id, {
+                                      isVerified: true,
+                                    })
+                                  }
+                                >
+                                  Mark as Verified
+                                </a>
+                              </li>
+                            )}
+
+                            {user.role === "admin" ? (
+                              <>
+                                <li>
+                                  <a
+                                    onClick={() =>
+                                      handleUpdateUser(user._id, {
+                                        role: "developer",
+                                      })
+                                    }
+                                  >
+                                    Demote to Developer
+                                  </a>
+                                </li>
+                                <li>
+                                  <a
+                                    onClick={() =>
+                                      handleUpdateUser(user._id, {
+                                        role: "client",
+                                      })
+                                    }
+                                  >
+                                    Demote to Client
+                                  </a>
+                                </li>
+                              </>
+                            ) : (
+                              <>
+                                <li>
+                                  <a
+                                    onClick={() =>
+                                      handleUpdateUser(user._id, {
+                                        role: "admin",
+                                      })
+                                    }
+                                  >
+                                    Promote to Admin
+                                  </a>
+                                </li>
+                                {user.role === "client" && (
+                                  <li>
+                                    <a
+                                      onClick={() =>
+                                        handleUpdateUser(user._id, {
+                                          role: "developer",
+                                        })
+                                      }
+                                    >
+                                      Change to Developer
+                                    </a>
+                                  </li>
+                                )}
+                                {user.role === "developer" && (
+                                  <li>
+                                    <a
+                                      onClick={() =>
+                                        handleUpdateUser(user._id, {
+                                          role: "client",
+                                        })
+                                      }
+                                    >
+                                      Change to Client
+                                    </a>
+                                  </li>
+                                )}
+                              </>
+                            )}
+
+                            <div className="divider my-1"></div>
+                            {user.accountStatus === "active" ? (
+                              <li>
+                                <a
+                                  className="text-error"
+                                  onClick={() =>
+                                    handleUpdateUser(user._id, {
+                                      accountStatus: "banned",
+                                    })
+                                  }
+                                >
+                                  Ban User
+                                </a>
+                              </li>
+                            ) : (
+                              <li>
+                                <a
+                                  className="text-success"
+                                  onClick={() =>
+                                    handleUpdateUser(user._id, {
+                                      accountStatus: "active",
+                                    })
+                                  }
+                                >
+                                  Unban User
+                                </a>
+                              </li>
+                            )}
+                          </ul>
+                        )}
+                      </div>
+                    </th>
+                  </tr>
+                );
+            })}
           </tbody>
         </table>
       </div>
