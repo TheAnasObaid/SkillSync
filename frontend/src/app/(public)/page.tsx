aef3549e08503ba05af2fe4e55ea127cf859e67d
Anas Obaid <anasobaidpk@gmail.com>
2025-08-01 04:46:38 +0500

    Build a layout page for public pages

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -7,7 +7,7 @@ import Workflow from "@/components/Home/Workflow";
 export default function Home() {
   return (
     <div className="bg-base-100 text-base-content font-sans grid">
-      <main className="max-w-6xl mx-auto px-4 py-16">
+      <main className="px-4 py-16">
         {/* <Hero />
         <Benefits />
         <Workflow />
