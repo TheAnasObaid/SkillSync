912d66848a45e958f78c401efefc456cfc78fb8d
Anas Obaid <anasobaidpk@gmail.com>
2025-07-31 14:53:49 +0500

    Apply emerald theme from Daisy

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      data-theme="skillsync-lol"
+      data-theme="emerald"
       className={`${inter.className} ${inter.variable}`}
     >
       <body className="antialiased">
