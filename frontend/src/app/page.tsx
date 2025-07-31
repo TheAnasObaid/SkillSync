8df26099a1bf5daaccd9ca9566a85782a2de3e9f
Anas Obaid <anasobaidpk@gmail.com>
2025-07-31 15:24:34 +0500

    Simply home temp

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <div className="bg-base-100 text-base-content font-sans grid">
       <main className="max-w-6xl mx-auto px-4 py-16">
-        <Hero />
+        {/* <Hero />
         <Benefits />
         <Workflow />
-        <FounderQuote />
+        <FounderQuote /> */}
         <CTA />
       </main>
     </div>
