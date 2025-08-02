6d5eb89571283540c040c19a0dfc03cc0ffd5e37
Anas Obaid <anasobaidpk@gmail.com>
2025-08-02 20:13:35 +0500

    Make the toast to be consistent with our colors

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import { FiCheckCircle, FiXCircle, FiLoader } from "react-icons/fi";
 
 export const metadata: Metadata = {
   title: "SkillSync",
@@ -23,8 +24,36 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="skillsync-pro">
       <body className={`${inter.className} ${inter.variable} antialiased`}>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          toastOptions={{
+            className:
+              "font-semibold rounded-full shadow-lg flex items-center gap-2 px-4 py-2",
+            success: {
+              icon: <FiCheckCircle size={20} />,
+              style: {
+                color: "#00fb7f",
+                background: "#00fb7f1a",
+              },
+            },
+            error: {
+              style: {
+                color: "#ff6266",
+                background: "#ff62661a",
+              },
+              icon: <FiXCircle size={20} />,
+            },
+            loading: {
+              style: {
+                color: "#fcb700",
+                background: "#fcb7001a",
+              },
+              icon: <FiLoader size={20} className="animate-spin" />,
+            },
+          }}
+        />
         <SocketProvider>{children}</SocketProvider>
-        <Toaster />
       </body>
     </html>
   );
