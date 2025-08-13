07157d5d7e6e7513e016ab5e2a463169bd8448eb
Anas Obaid <anasobaidpk@gmail.com>
2025-08-13 21:44:13 +0500

    Bring config and db connection to Next.js

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,23 @@
+import "server-only";
+
+const config = {
+  clientUrl: process.env.CLIENT_URL,
+  serverUrl: process.env.NEXT_PUBLIC_API_URL,
+  mongoUri: process.env.MONGO_URI,
+  jwtSecret: process.env.JWT_SECRET,
+  email: {
+    host: process.env.EMAIL_HOST,
+    port: parseInt(process.env.EMAIL_PORT || "587", 10),
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+    from: process.env.EMAIL_FROM || '"SkillSync" <noreply@skillsync.app>',
+  },
+};
+
+if (!config.mongoUri || !config.jwtSecret || !config.clientUrl) {
+  throw new Error(
+    "FATAL ERROR: A required environment variable (MONGO_URI, JWT_SECRET, or CLIENT_URL) is missing."
+  );
+}
+
+export default config;
